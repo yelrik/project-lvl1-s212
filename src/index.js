808d3b195706382528415e29537a3717ae852cae
aleks <2@2.ru>
2017-12-13 15:16:11 +0300

    update

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import readlineSync from 'readline-sync';
 
 const getUserName = () => {
   const name = readlineSync.question('May I have your name? ');
-  console.log(`Hello,  + ${name} + !`);
+  console.log(`Hello, ${name}!`);
 };
 export default getUserName;
+
+export const greeting = () => console.log('Welcome to the Brain Games!');
