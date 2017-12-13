ee8a2c81e2e9380b10317e084a260fbc2dd1a294
aleks <2@2.ru>
2017-12-13 20:46:54 +0300

    refactoring

--- src/bin/brain-even.js
+++ src/bin/brain-even.js
@@ -1,33 +1,5 @@
 #!/usr/bin/env node
-import readlineSync from 'readline-sync';
-import { greeting } from '..';
-
-const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min)) + min;
-const num = () => getRandomInt(1, 100);
-const isEven = n => ((n % 2 === 0) ? 'yes' : 'no');
-
-const getNum = () => getRandomInt(1, 100);
-
-const func = () => {
-  greeting();
-  console.log('Answer "yes" if number even otherwise answer "no".\n');
-  const name = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${name}! \n`);
-  let i = 0;
-  while (i < 3) {
-    const number = getNum();
-    console.log(`Question: ${number}`);
-    const ans = readlineSync.question('Your answer: ');
-    if (isEven(num) === ans) {
-      console.log('Correct!');
-      i += 1;
-    } else {
-      console.log(`'${ans}' is wrong answer \n Let's try again, ${name}!`);
-      return;
-    }
-  }
-  console.log(`Congratulations, ${name}!`);
-};
+import { func } from '..';
 
 func();
 
--- src/index.js
+++ src/index.js
@@ -4,6 +4,42 @@ const getUserName = () => {
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
 };
-export default getUserName;
 
 export const greeting = () => console.log('Welcome to the Brain Games!');
+
+export const getRandomInt = (min = 1, max = 100) => Math.floor(Math.random() * (max - min)) + min;
+
+export const isEven = n => ((n % 2 === 0) ? 'yes' : 'no');
+
+export const whatIsYourName = () => {
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}! \n`);
+  return name;
+};
+
+export const questionAndAnswer = (q) => {
+  console.log(`Question: ${q}`);
+  const ans = readlineSync.question('Your answer: ');
+  return ans;
+};
+
+export const func = () => {
+  greeting();
+  console.log('Answer "yes" if number even otherwise answer "no".\n');
+  const name = whatIsYourName();
+  let i = 0;
+  while (i < 3) {
+    const q = getRandomInt();
+    const ans = questionAndAnswer(q);
+    if (isEven(q) === ans) {
+      console.log('Correct!');
+      i += 1;
+    } else {
+      console.log(`'${ans}' is wrong answer \n Let's try again, ${name}!`);
+      return;
+    }
+  }
+  console.log(`Congratulations, ${name}!`);
+};
+
+export default getUserName;
