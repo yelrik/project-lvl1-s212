2691a089532442608608831014d1f0f14f83318f
aleks <2@2.ru>
2017-12-14 09:29:05 +0300

    update

--- src/bin/brain-even.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env node
-import { brainEven } from '..';
-
-brainEven();
-
--- src/bin/brain-games.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env node
-import getUserName, { greeting } from '..';
-
-greeting();
-getUserName();
